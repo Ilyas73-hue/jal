05b0fb160183669f6a5724466f6d4064154fd6c2
Ilyas73-hue <ilyasfullstackdeveloper@gmail.com>
2025-02-03 15:11:09 +0530

    some changes

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -56,7 +56,7 @@ function Contact() {
                                 whileInView={'show'}
                                 viewport={{ once: false, amount: 0.7 }}
             id="contact-section-2-div-1-2-1">
-            <iframe id="contact-section-2-div-1-2-1-g-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.8086393894537!2d77.34501747478132!3d9.081190590982242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b068536c442f879%3A0x6487bc05065ff30d!2sJAL%20NEET%20ACADEMY!5e0!3m2!1sen!2sin!4v1734281870786!5m2!1sen!2sin"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe id="contact-section-2-div-1-2-1-g-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.914309686449!2d77.72529617500808!3d8.699687594056869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b041361c4018091%3A0x16842f5b25edd9d1!2sJAL%20NEET%20ACADEMY!5e0!3m2!1sen!2sin!4v1738562826758!5m2!1sen!2sin"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </motion.div>
             <motion.div
             variants={matches ? fadeSmallIn("left") :fadeBigIn("left")}
